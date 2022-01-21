d812de6c84e43c252ac412e9083b5add3e67c54d
lastheroplan@outlook.com <lastheroplan@outlook.com>
2022-01-22 02:24:14 +0700

    Otladka konstruktora

--- CApplication/Header.h
+++ CApplication/Header.h
@@ -36,65 +36,65 @@ T2& Pair<T1, T2>::second()
 using ArraiInt = std::valarray<int>;
 using PairArray = Pair<ArraiInt, ArraiInt>;
 
-class Wine
+class Wine :private std::valarray<int>, std::string
 {
 private:
-	std::string label;
+	//std::string label;
     int ColYear;
-    PairArray YearCount;
+    //PairArray YearCount;
 public:
-    Wine(const char* l, int y, const int yr[], const int bot[]);
+    //Wine(const char* l, int y, const int yr[], const int bot[]);
     Wine(const char* l, int y);
     ~Wine() { }
-    void GetBottles();
-	void Show() const;
-    std::string Label() { return label; }
-	int const sum();
+ //   void GetBottles();
+	//void Show() const;
+    std::string Label() { return (const std::string&)(*this); }
+	//int const sum();
 };
 
-Wine::Wine(const char* l, int y, const int yr[], const int bot[]) :
-    label(l), ColYear(y)
+//Wine::Wine(const char* l, int y, const int yr[], const int bot[]) :
+//    label(l), ColYear(y)
+//{
+//    ArraiInt z(y);
+//     YearCount = PairArray(z, z);
+//   for (int i = 0; i < ColYear; ++i)
+//    {
+//       YearCount.first()[i] = yr[i];
+//       YearCount.second()[i] = bot[i];
+//   }
+//
+//}
+Wine::Wine(const char* l, int y):std::string(l), ColYear(y)
 {
     ArraiInt z(y);
-     YearCount = PairArray(z, z);
-   for (int i = 0; i < ColYear; ++i)
-    {
-       YearCount.first()[i] = yr[i];
-       YearCount.second()[i] = bot[i];
-   }
-
+    *this = Pair<ArraiInt, ArraiInt>(z, z);
 }
-Wine::Wine(const char* l, int y):label(l), ColYear(y)
-{
-    ArraiInt z(y);
-    YearCount = Pair<ArraiInt, ArraiInt>(z, z);
-}
-void Wine::GetBottles()
-{
-    for (int i = 0; i < ColYear; ++i)
-    {
-        std::cout << "Enter year: ";
-        int ttmp;
-        std::cin >> ttmp;
-        YearCount.first()[i] = ttmp;
-        std::cout << "Enter bottles for that year: ";
-        std::cin >> ttmp;
-        YearCount.second()[i] = ttmp;
-    }
-}
-void Wine::Show() const
-{
-    std::cout << "Wine: " << label << endl;
-    std::cout << "\tYear" << "\tBottles" << endl;;
-    for (int i = 0; i < ColYear; ++i)
-    {
-        std::cout << "\t" << YearCount.first()[i] << "\t" << YearCount.second()[i] << endl;;
-    }
-}
-int const Wine::sum()
-{
-    return YearCount.second().sum();
-}
+//void Wine::GetBottles()
+//{
+//    for (int i = 0; i < ColYear; ++i)
+//    {
+//        std::cout << "Enter year: ";
+//        int ttmp;
+//        std::cin >> ttmp;
+//        YearCount.first()[i] = ttmp;
+//        std::cout << "Enter bottles for that year: ";
+//        std::cin >> ttmp;
+//        YearCount.second()[i] = ttmp;
+//    }
+//}
+//void Wine::Show() const
+//{
+//    std::cout << "Wine: " << label << endl;
+//    std::cout << "\tYear" << "\tBottles" << endl;;
+//    for (int i = 0; i < ColYear; ++i)
+//    {
+//        std::cout << "\t" << YearCount.first()[i] << "\t" << YearCount.second()[i] << endl;;
+//    }
+//}
+//int const Wine::sum()
+//{
+//    return YearCount.second().sum();
+//}
 
 
 #endif
