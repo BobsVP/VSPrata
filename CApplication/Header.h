5d0ac93e8893b41b4e65352ff391938b9f3db5c5
lastheroplan@outlook.com <lastheroplan@outlook.com>
2022-02-02 01:28:29 +0700

    Exercize 14-4

--- CApplication/Header.h
+++ CApplication/Header.h
@@ -2,23 +2,159 @@
 #ifndef Header_h
 #define Header_h
 #include<iostream>
+#include <cstdlib> // для функций rand() и srand()
+#include <ctime> // для функции time()
+#include<string>
 using std::cout;
 using std::endl;
 
-template<typename T>
-class ManyFriendT
+//class Person
+class Person
 {
-private:
-	T item;
 public:
-	ManyFriendT(const T& i) :item(i) { }
-	template<typename C, typename D> friend void show2(C&, D&);
+	Person() {};
+	Person(const char* nname, const char* ffamily)
+		:Name(nname), Family(ffamily) {};
+	virtual ~Person() {};
+	virtual void Set();
+//protected:
+	virtual void Show();
+private:
+	std::string Name;
+	std::string Family;
 };
 
-template<typename C, typename D> void show2(C& c, D& d)
+void Person::Show()
 {
-	cout << c.item << ", " << d.item << endl;
+	cout << "Family: " << Family << endl;
+	cout << "Name: " << Name << endl;
+}
+void Person::Set()
+{
+	cout << "Enter Name: ";
+	std::cin >> Name;
+	cout << "Enter Family: ";
+	std::cin >> Family;
 }
 
+//class Gunslinger
+class Gunslinger : virtual public Person
+{
+public:
+	Gunslinger() {};
+	Gunslinger(const char* nname, const char* ffamily, int notches)
+	: Person(nname, ffamily), NumberNotchesRifle(notches), StartToGun(rand() % 100) {};
+	~Gunslinger() {};
+	double& Draw() { return StartToGun; }
+	void Show();
+	void Set();
+private:
+	double StartToGun;
+	int NumberNotchesRifle;
+};
+
+ void Gunslinger::Show()
+{
+	Person::Show();
+	cout << "Transition time to combat readiness: " << StartToGun << endl;
+	cout << "Number of notches on a rifle: " << NumberNotchesRifle << endl;
+}
+ void Gunslinger::Set()
+ {
+	 Person::Set();
+	 cout << "Enter transition time to combat readiness: ";
+	 std::cin >> StartToGun;
+	 cout << "Enter number of notches on a rifle: ";
+	 std::cin >> NumberNotchesRifle;
+ }
+
+ //class Card
+ class Card
+ {
+public:
+	Card(int a, int b) { SetSuitRank(a, b); };
+	Card(int a) { SetSuitRank(a % 13, a % 4); };
+	Card() { rank = "Joker,"; suit = " Joker"; }
+	~Card() {};
+	std::string& GetSuit() { return suit; }
+	std::string& GetRank() { return rank; }
+	void SetSuitRank(int a, int b);
+	void Show() { cout << "Playing card: " << rank << suit << endl; }
+ private:
+	std::string suit;
+	//enum class Suits {Diamonds, Hearts, Clubs, Spades};
+	std::string rank;
+	//enum class Ranks { Ace, King, Queen, Jack, ten, nine, eight, seven, six, five, four, three, deuce};
+};
+ void Card::SetSuitRank(int a, int b)
+ {
+	 std::string Ranks[13] = { "Ace of ", "King of ", "Queen of ", "Jack of ", "ten of ", "nine of ",
+		  "eight of ", "seven of ", "six of ", "five of ", "four of ", "three of ", "deuce of " };
+	 std::string Suits[4] = { "Diamonds", "Hearts", "Clubs", "Spades" };
+	 rank = Ranks[a];
+	 suit = Suits[b];
+ }
+
+//class PokerPlayer
+class PokerPlayer : virtual public Person
+{
+public:
+
+	PokerPlayer(const char* nname, const char* ffamily) 
+		: Person(nname, ffamily)  {}; 
+	PokerPlayer() {};
+	~PokerPlayer() {};
+	Card Draw() { Card tmp(rand() % 53); return tmp; } 
+	void Show();
+	void Set();
+	Card& GetTrump() { return Trump; }
+private:
+	Card Trump;
+};
+void PokerPlayer::Set()
+{
+	Person::Set();
+	Trump = Draw();
+}
+void PokerPlayer::Show() 
+{ 
+	Person::Show(); 
+	Trump.Show();
+}
+
+class BadDude : public Gunslinger, public PokerPlayer
+{
+public:
+	BadDude() {};
+	BadDude(const char* nname, const char* ffamily):Person(nname, ffamily) {};
+	~BadDude() {};
+	void Show();
+	double Gdraw();
+	Card Cdraw();
+	void Set();
+private:
+};
+
+void BadDude::Set()
+{
+	Person::Set();
+	cout << "Enter transition time to combat readiness: ";
+	std::cin >> Gunslinger::Draw();
+	PokerPlayer::GetTrump() = Cdraw();
+}
+void BadDude::Show()
+{
+	Person::Show();
+	cout << "Transition time to combat readiness: " << Gdraw() << endl;
+	PokerPlayer::GetTrump().Show();
+}
+double BadDude::Gdraw()
+{
+	return Gunslinger::Draw();
+}
+Card BadDude::Cdraw()
+{
+	return PokerPlayer::Draw();
+}
 
 #endif
--- CApplication/Sour.cpp
+++ CApplication/Sour.cpp
@@ -1,18 +1,56 @@
 ﻿#include"Header.h"
-#include<iostream>
-using std::cout;
-using std::cin;
-using std::endl;
+#include <iostream>
+#include <cstring>
+const int SIZE = 5;
 
 int main()
 {
-	ManyFriendT<int> hfi1(10);
-	ManyFriendT<int> hfi2(20);
-	ManyFriendT<double> hfdb(10.5);
-	cout << "hfi1, hfi2:\n";
-	show2(hfi1, hfi2);
-	cout << "hfdb, hfi2:\n";
-	show2(hfdb, hfi2);
-	return 0;
-}
+    using std::cin;
+    using std::cout;
+    using std::endl;
+    using std::strchr;
+    srand(time(0));
+    Person* lolas[SIZE];
 
+    int ct;
+    for (ct = 0; ct < SIZE; ct++)
+    {
+        char choice;
+        cout << "Enter the klass category:\n"
+            << "p: Person  g: Gunslinger l: PokerPlayer  "
+            << "b: BadDude  q: quit\n";
+        cin >> choice;
+        while (strchr("pglbq", choice) == NULL)
+        {
+            cout << "Please enter a p, g, l, b or q: ";
+            cin >> choice;
+        }
+        if (choice == 'q')
+            break;
+        switch (choice)
+        {
+        case 'p':   lolas[ct] = new Person;
+            break;
+        case 'g':   lolas[ct] = new Gunslinger;
+            break;
+        case 'l':   lolas[ct] = new PokerPlayer;
+            break;
+        case 'b':   lolas[ct] = new BadDude;
+            break;
+        }
+        cin.get();
+        lolas[ct]->Set();
+    }
+
+    cout << "\nHere is your staff:\n";
+    int i;
+    for (i = 0; i < ct; i++)
+    {
+        cout << endl;
+        lolas[i]->Show();
+    }
+    for (i = 0; i < ct; i++)
+        delete lolas[i];
+    cout << "Bye.\n";
+    return 0;
+}
