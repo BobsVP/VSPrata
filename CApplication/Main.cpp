8ec10c0c0f4a7b0a87a525410f5da04294a040f4
lastheroplan@outlook.com <lastheroplan@outlook.com>
2022-05-08 18:21:47 +0700

    Prata exercise 16-4

--- CApplication/Main.cpp
+++ CApplication/Main.cpp
@@ -1,105 +1,19 @@
 ﻿#include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <cctype>
-using std::string;
-//const int NUM = 26;
-std::ifstream file_slovar;
+#include <algorithm>
 
-//const string wordlist[NUM] = { "apiary", "beetle", "cereal",
-//    "danger", "ensign", "florid", "garage", "health", "insult",
-//    "jackal", "keeper", "loaner", "manage", "nonce", "onset",
-//    "plaid", "quilt", "remote", "stolid", "train", "useful",
-//    "valid", "whence", "xenon", "yearn", "zippy" };
+int reduce(long ar[], int n);
 
 int main()
 {
-    using std::cout;
-    using std::cin;
-    using std::tolower;
-    using std::endl;
-    file_slovar.open("Slovar.txt");
-    if (!file_slovar)
-    {
-        std::cout << "Error openning file!";
-        return -1;
-    }
-    std::string word;
-    std::vector<std::string> wordlist;
-    while (!file_slovar.eof())
-    {
-        file_slovar >> word;
-        wordlist.push_back(word);
-    }
-    int NUM = wordlist.size();
-    std::srand(std::time(0));
-    char play;
-    cout << "Will you play a word game? <y/n> ";
-    cin >> play;
-    play = tolower(play);
-    while (play == 'y')
-    {
-        string target = wordlist[std::rand() % NUM];
-        int length = target.length();
-        string attempt(length, '-');
-        string badchars;
-        int guesses = 6;
-        cout << "Guess my secret word. It has " << length
-            << " letters, and you guess\n"
-            << "one letter at a time. You get " << guesses
-            << " wrong guesses.\n";
-        cout << "Your word: " << attempt << endl;
-        while (guesses > 0 && attempt != target)
-        {
-            char letter;
-            cout << "Guess a letter: ";
-            cin >> letter;
-            if (badchars.find(letter) != string::npos
-                || attempt.find(letter) != string::npos)
-            {
-                cout << "You already guessed that. Try again.\n";
-                continue;
-            }
-            int loc = target.find(letter);
-            if (loc == string::npos)
-            {
-                cout << "Oh, bad guess!\n";
-                --guesses;
-                badchars += letter; // add to string
-            }
-            else
-            {
-                cout << "Good guess!\n";
-                attempt[loc] = letter;
-                // check if letter appears again
-                loc = target.find(letter, loc + 1);
-                while (loc != string::npos)
-                {
-                    attempt[loc] = letter;
-                    loc = target.find(letter, loc + 1);
-                }
-            }
-            cout << "Your word: " << attempt << endl;
-            if (attempt != target)
-            {
-                if (badchars.length() > 0)
-                    cout << "Bad choices: " << badchars << endl;
-                cout << guesses << " bad guesses left\n";
-            }
-        }
-        if (guesses > 0)
-            cout << "That's right!\n";
-        else
-            cout << "Sorry, the word is " << target << ".\n";
-
-        cout << "Will you play another? <y/n> ";
-        cin >> play;
-        play = tolower(play);
-    }
-    cout << "Bye\n";
-    file_slovar.close();
+    long ar_main[19] = { 1,2,3,4,5,6,7,8,9,9,8,7,6,5,4,3,2,1,0 };
+    std::cout << reduce(ar_main, 19) << std::endl;
     return 0;
+}
+
+int reduce(long ar[], int n)
+{
+    std::sort(ar, ar + n);
+    auto an = std::unique(ar, ar + n);
+    return an - ar;
 }
