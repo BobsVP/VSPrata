15cd1f84cf29faf0fd98629d8ff827128d0f7add
lastheroplan@outlook.com <lastheroplan@outlook.com>
2022-05-08 18:45:15 +0700

    Prata exercise 16-5

--- CApplication/Main.cpp
+++ CApplication/Main.cpp
@@ -1,19 +1,21 @@
 ﻿#include <iostream>
-#include <vector>
+#include <string>
 #include <algorithm>
 
-int reduce(long ar[], int n);
-
-int main()
-{
-    long ar_main[19] = { 1,2,3,4,5,6,7,8,9,9,8,7,6,5,4,3,2,1,0 };
-    std::cout << reduce(ar_main, 19) << std::endl;
-    return 0;
-}
-
-int reduce(long ar[], int n)
+template<typename T>
+int reduce(T ar[], int n)
 {
     std::sort(ar, ar + n);
     auto an = std::unique(ar, ar + n);
     return an - ar;
 }
+
+int main()
+{
+    long ar_main[19] = { 1,2,3,4,5,6,7,8,9,9,8,7,6,5,4,3,2,1,0 };
+    std::string ar_strng[19] = { "a", "b", "c", "d", "e", "f", "g", "h", "a", "b", "c", "d", "a", "f", "g", "h", "i", "j", "k" };
+    std::cout << reduce(ar_main, 19) << std::endl;
+    std::cout << reduce(ar_strng, 19) << std::endl;
+    return 0;
+}
+
