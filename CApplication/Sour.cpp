e93f7f8594a7f510b4f9e42a3ac0081a48654e26
Vladimir P <pustovalovv455@gmail.com>
2022-09-19 01:15:02 +0700

    Exercise 17-1

--- CApplication/Sour.cpp
+++ CApplication/Sour.cpp
@@ -1,18 +1,26 @@
-﻿#include"Header.h"
+﻿//#include"Header.h"
 #include<iostream>
 using std::cout;
 using std::cin;
 using std::endl;
+const int Limit = 255;
 
 int main()
 {
-	ManyFriendT<int> hfi1(10);
-	ManyFriendT<int> hfi2(20);
-	ManyFriendT<double> hfdb(10.5);
-	cout << "hfi1, hfi2:\n";
-	show2(hfi1, hfi2);
-	cout << "hfdb, hfi2:\n";
-	show2(hfdb, hfi2);
+	char input[Limit];
+	cout << "Enter a string to count the number of characters:" << endl;
+	cin.get(input, Limit, '$');
+	int count = 0;
+	for (size_t i = 0; i < Limit; ++i)
+	{
+		if (input[i] != '\0')
+			count++;
+		else
+			break;
+	}
+	char ch;
+	cin.get(ch);
+	cout << "You entered the " << count << " of characters" << endl << ch;
 	return 0;
 }
 
