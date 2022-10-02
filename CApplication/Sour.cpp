f4ed13c5fd6366208c94da63402931b51fbd43df
pustovalovv455@gmail.com <pustovalovv455@gmail.com>
2022-10-03 03:04:18 +0700

    Prata exercise 17-4

--- CApplication/Sour.cpp
+++ CApplication/Sour.cpp
@@ -1,39 +1,55 @@
 ﻿//#include"Header.h"
 #include<iostream>
 #include<fstream>
+#include<string>
 using std::cout;
 using std::cin;
 using std::endl;
 
 int main(int argc, char * argv[])
 {
-	if (argc < 3)
+	if (argc < 4)
 	{
 		std::cerr << "Too few command line options found. Execution terminated." << std::endl;
 		std::cin.get(); 
 		exit(EXIT_FAILURE);
 	}
-	std::ifstream File1(argv[1]);
-	if (!File1.is_open())
+	std::ifstream FileInput1(argv[1]);
+	if (!FileInput1.is_open())
 	{
-		std::cerr << "\nCould not open input file. Execution terminated." << std::endl;
+		std::cerr << "\nCould not open input file#1. Execution terminated." << std::endl;
 		std::cin.get();
 		exit(EXIT_FAILURE);
 	}
-	std::ofstream File2(argv[2]);
-	if (!File2.is_open())
+	std::ifstream FileInput2(argv[2]);
+	if (!FileInput2.is_open())
+	{
+		std::cerr << "\nCould not open intput file#2. Execution terminated." << std::endl;
+		std::cin.get();
+		exit(EXIT_FAILURE);
+	}
+	std::ofstream FileOutput(argv[3]);
+	if (!FileOutput.is_open())
 	{
 		std::cerr << "\nCould not open output file. Execution terminated." << std::endl;
 		std::cin.get();
 		exit(EXIT_FAILURE);
 	}
-	char ch;
-	while (File1.get(ch))
+	std::string str;
+	while (!FileInput1.eof() || !FileInput2.eof())
 	{
-		File2 << ch;
+		if (getline(FileInput1, str))
+			FileOutput << str;
+		if (!FileInput1.eof() && !FileInput2.eof())
+			FileOutput << " ";
+		if (getline(FileInput2, str))
+			FileOutput << str;
+		if (!FileInput1.eof() || !FileInput2.eof())
+			FileOutput << endl;
 	}
-	File1.close();
-	File2.close();
+	FileInput1.close();
+	FileInput2.close();
+	FileOutput.close();
 	return 0;
 }
 
