101a85febc78b8ba549c5b5b4ba6833a1567b0b2
pustovalovv455@gmail.com <pustovalovv455@gmail.com>
2022-10-16 23:29:37 +0700

    added delete

--- CApplication/Sour.cpp
+++ CApplication/Sour.cpp
@@ -127,6 +127,7 @@ int main(int argc, char * argv[])
 						TypeEntry = 6;
 					FileData << TypeEntry << endl;
 					pc[i]->WriteAll(FileData);
+					delete pc[i];
 				}
 			}
 			FileData.close();
