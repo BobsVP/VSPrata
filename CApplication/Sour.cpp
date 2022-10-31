1d52908649266ff6044bfd63f5c231983c6f1d2c
pustovalovv455@gmail.com <pustovalovv455@gmail.com>
2022-10-31 20:27:44 +0700

    Exercise 17-7

--- CApplication/Sour.cpp
+++ CApplication/Sour.cpp
@@ -1,199 +1,67 @@
-﻿#include"Header.h"
-#include<iostream>
-#include<fstream>
+﻿#include<iostream>
 #include<string>
-using std::cout;
-using std::cin;
-using std::endl;
-const int MAX = 10;
+#include<vector>
+#include<algorithm>
+#include<fstream>
 
-void ShowEntry(abstr_emp** pc);
-int DeleteEntry(int s, abstr_emp** pc);
-int ininput(int start, int end);
-int FreeIndex(abstr_emp* pc[]);
-
-int main(int argc, char * argv[])
+void ShowStr(const std::string& s);
+class Store
 {
-	const std::string fileName = "employees.dat";
-	abstr_emp* pc[MAX]{ nullptr };
-	std::ifstream FileData(fileName);
-	if (FileData.is_open())
+public:
+	Store(std::ofstream& osin):os(osin) {}
+	void operator()(const std::string& str) 
 	{
-		int classtype;
-		int counter = 0;
-		while (FileData >> classtype && (classtype > 4 && classtype < 9))
-		{
-			FileData.get();
-			switch (classtype)
-			{
-			case 5: pc[counter] = new employee;
-				pc[counter]->GetAll(FileData);
-				break;
-			case 6:pc[counter] = new manager;
-				pc[counter]->GetAll(FileData);
-				break;
-			case 7:pc[counter] = new fink;
-				pc[counter]->GetAll(FileData);
-				break;
-			case 8:pc[counter] = new highfink;
-				pc[counter]->GetAll(FileData);
-				break;
-			}
-			counter++;
-		}
-		cout << "The file " << fileName << " contains the following entries : \n";
-		ShowEntry(pc);
+		size_t len = str.length();
+		os.write((char*)&len, sizeof(std::size_t));
+		os.write(str.data(), len);
 	}
-	FileData.close();
-	bool repeat = true;
-	while (repeat)
+private:
+	std::ofstream& os;
+};
+void GetStrs(std::ifstream& is, std::vector<std::string>& vis);
+
+int main()
+{
+	using namespace std;
+	vector<string> vostr;
+	string temp;
+	cout << "Enter strings (empty line to quit):\n";
+	while (getline(cin, temp) && temp[0] != '\0')
+		vostr.push_back(temp);
+	cout << "Here is you input.\n";
+	for_each(vostr.begin(), vostr.end(), ShowStr);
+	ofstream fout("strings.dat", ios_base::out | ios_base::binary);
+	for_each(vostr.begin(), vostr.end(), Store(fout));
+	fout.close();
+	vector<string> vistr;
+	ifstream fin("strings.dat", ios_base::in | ios_base::binary);
+	if (!fin.is_open())
 	{
-		cout << "What do you want to do?" << endl;
-		cout << "1. Add new entry" << endl;
-		cout << "2. Delete entry" << endl;
-		cout << "3. Show current entries" << endl;
-		cout << "4. Save & Exit" << endl;
-		cout << ">";
-		int s;
-		s = ininput(1, 4);
-		switch (s)
-		{
-		case 1: cout << "What type to create?" << endl;
-			cout << "5. Employee" << endl;
-			cout << "6. Manger" << endl;
-			cout << "7. Fink" << endl;
-			cout << "8. Highfink" << endl;
-			s = ininput(5, 8);
-			switch (s)
-			{
-				int index;
-			case 5:index = FreeIndex(pc);
-				if (index != -1)
-				{
-					pc[index] = new employee;
-					pc[index]->SetAll();
-				}
-				else
-					std::cerr << "No free cell." << endl;
-				break;
-			case 6:index = FreeIndex(pc);
-				if (index != -1)
-				{
-					pc[index] = new manager;
-					pc[index]->SetAll();
-				}
-				else
-					std::cerr << "No free cell." << endl;
-				break;
-			case 7:index = FreeIndex(pc);
-				if (index != -1)
-				{
-					pc[index] = new fink;
-					pc[index]->SetAll();
-				}
-				else
-					std::cerr << "No free cell." << endl;
-				break;
-			case 8:index = FreeIndex(pc);
-				if (index != -1)
-				{
-					pc[index] = new highfink;
-					pc[index]->SetAll();
-				}
-				else
-					std::cerr << "No free cell." << endl;
-				break;
-			}
-			break;
-		case 2: cout << "Enter the number entry to delete: ";
-			s = ininput(1, 10);
-			if (DeleteEntry(s, pc) == -1)
-				std::cerr << "Bad number entry." << endl;
-			break;
-		case 3: ShowEntry(pc);
-			break;
-		case 4: repeat = false;
-			std::ofstream FileData(fileName);
-			for (int i = 0; i < MAX; i++)
-			{
-				if (pc[i] != nullptr)
-				{
-					int TypeEntry = 5;
-					if (dynamic_cast<highfink*>(pc[i]))
-						TypeEntry = 8;
-					else if (dynamic_cast<fink*>(pc[i]))
-						TypeEntry = 7;
-					else if (dynamic_cast<manager*>(pc[i]))
-						TypeEntry = 6;
-					FileData << TypeEntry << endl;
-					pc[i]->WriteAll(FileData);
-					delete pc[i];
-				}
-			}
-			FileData.close();
-			break;
-		}
+		cerr << "Could not opn file for input.\n";
+		exit(EXIT_FAILURE);
 	}
+	GetStrs(fin, vistr);
+	cout << "\nHere are the string read from the file:\n";
+	for_each(vistr.begin(), vistr.end(), ShowStr);
 	return 0;
 }
 
-int ininput(int start, int end)
+void ShowStr(const std::string& s)
 {
-	int num = 0;
-	while (num == 0)
+	std::cout << s << std::endl;
+}
+void GetStrs(std::ifstream& is, std::vector<std::string>& vis)
+{
+	char chr; 
+	size_t len;
+	while (is.read((char*)&len, sizeof(std::size_t)))
 	{
-		cin >> num;
-		if (cin.fail() && !cin.eof())
+		std::string str;
+		for (size_t i = 0; i < len; ++i)
 		{
-			cin.clear();
-			cin.ignore(32767, '\n');
-			std::cerr << "Incorrect input. Try again: ";
-			continue;
+			is.read(&chr, 1);
+			str.push_back(chr);
 		}
-		if (num < start || num > end)
-		{
-			std::cerr << "Incorrect choice. Try again: ";
-			num = 0;
-		}
-		cin.ignore(32767, '\n');
+		vis.push_back(str);
 	}
-	return num;
 }
-
-void ShowEntry(abstr_emp** pc)
-{
-	int counter = 0;
-	for (int x = 0; x < MAX; ++x)
-		if (*(pc + x) != nullptr)
-		{
-			counter++;
-			cout << "\t\tEntry #" << counter << endl;
-			pc[x]->ShowAll();
-		}
-	if (counter == 0)
-		std::cerr << "No entries. \n";
-}
-int FreeIndex(abstr_emp* pc[])
-{
-	for (int x = 0; x < MAX; ++x)
-		if (*(pc + x) == nullptr)
-			return x;
-	return -1;
-}
-int DeleteEntry(int s, abstr_emp** pc)
-{
-	int counter = 0;
-	for (int x = 0; x < MAX; ++x)
-		if (*(pc + x) != nullptr)
-		{
-			counter++;
-			if (counter == s)
-			{
-				delete *(pc + x);
-				*(pc + x) = nullptr;
-				return x;
-			}
-		}
-	return -1;
-}
-
